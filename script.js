156ed6295912f685df9ab6f6c9ef87650628c073
Sukhomlinov Nikita <nsm200216@gmail.com>
2022-11-07 01:39:01 +0300

    added 'for' for labeles

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.find').addEventListener('click', () => {
-    let elem = document.querySelector("#sm");
-    alert(elem.checked)
-    let c = document.querySelector("#check");
-    alert(c.checked)       
-});

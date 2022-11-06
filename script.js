f95854c5f2d5e744c1cab1da59733cf6e2248b1e
Sukhomlinov Nikita <nsm200216@gmail.com>
2022-11-06 16:38:57 +0300

    firs commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.find').addEventListener('click', () => {
+    // alert('loh');
+    let data = document.querySelector('.sm2').value;
+    alert(data);
+    alert();
+    document.querySelector('.out').innerHTML = data;
+});

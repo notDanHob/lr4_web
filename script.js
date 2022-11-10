4debd4244f54be9c3082b4b6b42bcffa20599275
UmovistovDenis <114919728+UmovistovDenis@users.noreply.github.com>
2022-11-10 22:40:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Coffee {
     let result_length = results.length;
     if (result_length == 0) {
       document.getElementById("description").innerHTML = "No monney for coffee";
-      return "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Стакан.jpg/200px-Стакан.jpg";
+        return "https://psv4.userapi.com/c534536/u98411440/docs/d5/b6cb5752cd90/No_Money.jpg?extra=Cbn9aYv5gCjQWnu4Wyj8dRDrdBVcyrqbAXWOWqmI-FBsXqPNyfyHghYhNSwZMXpF6snqijmtQ2l7SHD5GBcRYWInPdpbJ2I6aCmRJEBv3Enwi5a8kKN_D7q-ynBED2t8F7ubpTb9iYzlYDCFfZcD9Q";
     }
     for (let i = 0; i < this.my_coffee_list.length; i++){
       if (this.my_coffee_list[i]['ingredients'].length == result_length) {
@@ -34,7 +34,7 @@ class Coffee {
       }
     }
     document.getElementById("description").innerHTML = "Drink some shit";
-    return "https://memepedia.ru/wp-content/uploads/2018/09/papey-gavna-original.jpg";
+      return "https://memepedia.ru/wp-content/uploads/2018/09/papey-gavna-original.jpg";
   }
 }
 
